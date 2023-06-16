bb8875da4adfd95df20d598c27ad5810004dd3e5
Sparky <48359461+Sparky579@users.noreply.github.com>
2023-06-16 18:09:05 +0800

    Update asm.s

--- kernel/asm.s
+++ kernel/asm.s
@@ -16,6 +16,7 @@
 .globl invalid_TSS,segment_not_present,stack_segment
 .globl general_protection,coprocessor_error,irq13,reserved
 
+#ifdef __x86__
 divide_error:
 	pushl $do_divide_error
 no_error_code:
@@ -127,7 +128,78 @@ error_code:
 	popl %ebx
 	popl %eax
 	iret
+#elif __X64__
+divide_error:
+	pushq $do_divide_error
+no_error_code:
+	xchgq %rax,(%rsp)
+	pushq %rbx
+	pushq %rcx
+	pushq %rdx
+	pushq %rdi
+	pushq %rsi
+	pushq %rbp
+	push %ds
+	push %es
+	push %fs
+	pushq $0              # "error code"
+	lea 44(%rsp),%rdx
+	pushq %rdx
+	movq $0x10,%rdx
+	mov %dx,%ds
+	mov %dx,%es
+	mov %dx,%fs
+	call *%rax
+	addq $16,%rsp
+	pop %fs
+	pop %es
+	pop %ds
+	popq %rbp
+	popq %rsi
+	popq %rdi
+	popq %rdx
+	popq %rcx
+	popq %rbx
+	popq %rax
+	iretq
 
+double_fault:
+	pushq $do_double_fault
+error_code:
+	xchgq %rax,8(%rsp)       # error code <-> %rax
+	xchgq %rbx,(%rsp)        # &function <-> %rbx
+	pushq %rcx
+	pushq %rdx
+	pushq %rdi
+	pushq %rsi
+	pushq %rbp
+	push %ds
+	push %es
+	push %fs
+	pushq %rax               # error code
+	lea 44(%rsp),%rax        # offset
+	pushq %rax
+	movq $0x10,%rax
+	mov %ax,%ds
+	mov %ax,%es
+	mov %ax,%fs
+	call *%rbx
+	addq $16,%rsp
+	pop %fs
+	pop %es
+	pop %ds
+	popq %rbp
+	popq %rsi
+	popq %rdi
+	popq %rdx
+	popq %rcx
+	popq %rbx
+	popq %rax
+	iretq
+#endif
+
+
+#IFDEF __X86__
 invalid_TSS:
 	pushl $do_invalid_TSS
 	jmp error_code
@@ -143,4 +215,21 @@ stack_segment:
 general_protection:
 	pushl $do_general_protection
 	jmp error_code
+#elif __X64__
+invalid_TSS:
+	pushq $do_invalid_TSS
+	jmp error_code
+
+segment_not_present:
+	pushq $do_segment_not_present
+	jmp error_code
+
+stack_segment:
+	pushq $do_stack_segment
+	jmp error_code
+
+general_protection:
+	pushq $do_general_protection
+	jmp error_code
+#ENDIF
 
